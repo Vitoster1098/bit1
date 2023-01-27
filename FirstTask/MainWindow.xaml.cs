5a3f63cb1dd4ba4bb38a20c014f95cfd69e633db
Vitoster1098 <zzzvitzzz4@gmail.com>
2023-01-27 18:02:29 +0300

    feat: Правильный вывод + оптимизация

--- FirstTask/MainWindow.xaml.cs
+++ FirstTask/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Shapes;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Data.Common;
 
 namespace FirstTask
 {
@@ -41,14 +42,19 @@ namespace FirstTask
 
         public bool IsConnect()
         {
-            if (Connection == null)
+            if (String.IsNullOrEmpty(DatabaseName))
+                return false;
+            string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
+            Connection = new MySqlConnection(connstring);
+            try
             {
-                if (String.IsNullOrEmpty(DatabaseName))
-                    return false;
-                string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
-                Connection = new MySqlConnection(connstring);
                 Connection.Open();
             }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }            
 
             return true;
         }
@@ -64,10 +70,20 @@ namespace FirstTask
         /// </summary>
     public partial class MainWindow : Window
     {
+        DBConnection dbCon = DBConnection.Instance(); /* Класс соединения с БД */
+        int level = 0; /* 0 - города, 1 - улицы, 2 - дома, 3 - квартиры */
+        int id = 0; /* Id записи */
+
         public MainWindow()
         {
-            InitializeComponent();
-            FillGrid();
+            InitializeComponent();          
+
+            dbCon.Server = "127.0.0.1";
+            dbCon.DatabaseName = "wpf";
+            dbCon.UserName = "root";
+            dbCon.Password = "";
+
+            FillGrid(dbCon);
         }
 
         public class cities
@@ -84,35 +100,129 @@ namespace FirstTask
             }
         }
 
-        public void FillGrid()
+        public class streets
         {
-            var dbCon = DBConnection.Instance();
-            dbCon.Server = "127.0.0.1";
-            dbCon.DatabaseName = "wpf";
-            dbCon.UserName = "root";
-            dbCon.Password = "";
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Count { get; set; }
 
-            if(dbCon.IsConnect())
+            public streets(int Id, string Name, int Count)
             {
-                Console.WriteLine("Connected to db");
+                this.Id = Id;
+                this.Name = Name;
+                this.Count = Count;
+            }
+        }
 
-                string sql = "SELECT cities.id AS 'Id города', cities.name AS 'Название города', COUNT(streets.city_id) AS 'Количество улиц' " +
+        public class houses
+        {
+            public int Id { get; set; }
+            public string Number { get; set; }
+            public int Count { get; set; }
+
+            public houses(int Id, string Number, int Count)
+            {
+                this.Id = Id;
+                this.Number = Number;
+                this.Count = Count;
+            }
+        }
+
+        public class apartments
+        {
+            public int Id { get; set; }
+            public float area { get; set; }
+            public int houseId { get; set; }
+            
+            public apartments(int Id, float area, int houseId)
+            {
+                this.Id = Id;
+                this.area = area;
+                this.houseId = houseId;
+            }
+        }
+
+        /* Заполнить таблицу 1 уровня */
+        public void FillGrid(DBConnection dbCon)
+        {
+           string sql = "SELECT cities.id AS 'Id города', cities.name AS 'Название города', COUNT(streets.city_id) AS 'Количество улиц' " +
                 "FROM cities, streets " +
                 "WHERE cities.id = streets.city_id " +
                 "GROUP BY cities.id";
 
-                List<cities> citiesList = new List<cities>();
+            ExecuteQuery(sql);
+        }
 
-                var cmd = new MySqlCommand(sql, dbCon.Connection);
-                var reader = cmd.ExecuteReader();
+        /* Заполнить таблицу 2 уровня */
+        public void FillDrid2(DBConnection dbCon)
+        {
+            string sql = "SELECT streets.id AS 'Id улицы', streets.name AS 'Название улицы', COUNT(houses.id) AS 'Количество домов' " +
+                    "FROM streets, houses " +
+                    "WHERE streets.city_id = " + id + " AND houses.street_id = streets.id " +
+                    "GROUP BY streets.id;";
 
-                while (reader.Read())
-                {
-                    citiesList.Add(new cities(Convert.ToInt32(reader.GetString(0)), reader.GetString(1), Convert.ToInt32(reader.GetString(2))));
-                }
+            ExecuteQuery(sql);
+        }
 
-                Table.ItemsSource = citiesList;
+        /* Заполнить таблицу 3 уровня */
+        public void FillDrid3(DBConnection dbCon)
+        {
+
+        }
+
+        /* Заполнить таблицу 4 уровня */
+        public void FillDrid4(DBConnection dbCon)
+        {
+
+        }
+
+        public void ExecuteQuery(string sql)
+        {
+            if (dbCon.IsConnect())
+            {
+                DataSet dataSet = new DataSet();
+                DataAdapter dataAdapter = new MySqlDataAdapter(sql, dbCon.Connection.ConnectionString);
+                dataAdapter.Fill(dataSet);
+                Table.ItemsSource = dataSet.Tables[0].DefaultView;
+
                 dbCon.Close();
+            }
+            else
+            {
+                MessageBox.Show("Нет соединения с БД", "Ошибка");
+            }
+        }
+
+        /* Двойное нажатие по DataGrid для получения id строки */
+        private void Table_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var row = Table.SelectedItem as DataRowView;
+            //MessageBox.Show(row.Row.ItemArray[0].ToString());
+            id = Convert.ToInt32(row.Row.ItemArray[0]);
+            level += 1;
+
+            switch (level) 
+            {
+                case 0:
+                    {
+                        FillGrid(dbCon);
+                        break;
+                    }
+                case 1:
+                    {
+                        FillDrid2(dbCon); 
+                        break;
+                    }
+                case 2: 
+                    { 
+                        FillDrid3(dbCon);
+                        break;
+                    }
+                case 3:
+                    {
+                        FillDrid4(dbCon);
+                        break;
+                    }
             }
         }
     }
